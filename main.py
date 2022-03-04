2608604b8e2d5919555ce9795faa5309619e44a4
Eyüp Pastırmacı <pastirmacieyup7@gmail.com>
2022-03-04 18:07:30 +0300

    Example sentence fixed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     pre_process = PreProcess()
 
     # Örnek
-    turkish_sample = "1941 yılında astronomlar, 100 inç (2.500 mm)lik bir teleskop kullanarak süpernovadan arta kalan gaz kalıntısını tespit ettiler. Bu süpernovadan kaynaklanan kalıntı, türünün \"prototip\" nesnelerinden biri olarak kabul edilir ve hâlen astronomlarca üzerinde pek çok çalışmalar yapılmaktadır."
+    turkish_sample = '1941 yılında astronomlar, 100 inç (2.500 mm)lik bir teleskop kullanarak süpernovadan arta kalan gaz kalıntısını tespit ettiler. Bu süpernovadan kaynaklanan kalıntı, türünün "prototip" nesnelerinden biri olarak kabul edilir ve hâlen astronomlarca üzerinde pek çok çalışmalar yapılmaktadır.'
     print(turkish_sample)
     print("_____________________")
     print(pre_process.extract_stop_words(turkish_sample))
