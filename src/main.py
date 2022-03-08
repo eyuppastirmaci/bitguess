57545fca93f9a3368aba7fb54268140c432c5507
Eyüp Pastırmacı <pastirmacieyup7@gmail.com>
2022-03-09 01:30:01 +0300

    Main updates

--- src/main.py
+++ src/main.py
@@ -4,12 +4,14 @@ from data_file import CsvFile
 
 def main():
     meta_characters = ["rt ", "\n", "\t"]
+    csv_file_path = "data/turkish_tweets.csv"
+    column_index = 8
 
     pre_process = TweetPreProcess(meta_characters)
-    csv_file = CsvFile("data/turkish_tweets.csv", pre_process)
+    csv_file = CsvFile(csv_file_path, pre_process)
     csv_data_frame = csv_file.data_frame
     tweet_list = csv_data_frame.text.tolist()
-    csv_file.pre_process_column(csv_data_frame, tweet_list, 8)
+    csv_file.pre_process_column(csv_data_frame, tweet_list, column_index)
 
 
 if __name__ == '__main__':
