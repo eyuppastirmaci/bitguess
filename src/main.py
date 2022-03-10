674b794a11d961e5365e2d1d539c5025a53b2c48
Eyüp Pastırmacı <pastirmacieyup7@gmail.com>
2022-03-10 20:55:14 +0300

    Update main

--- src/main.py
+++ src/main.py
@@ -11,9 +11,9 @@ def main():
     out_file_path = "data/pre_processed-turkish_tweets.csv"
     encoding = "utf-8"
 
-    tweet_pre_process = TweetPreProcess(meta_characters=meta_characters)
-    csv_file = CsvFile(path=csv_file_path, pre_process=tweet_pre_process, out_path=out_file_path,
-                       encoding=encoding)
+    tweet_pre_process = TweetPreProcess(meta_characters)
+    csv_file = CsvFile(csv_file_path, tweet_pre_process, out_file_path,
+                       encoding)
     csv_data_frame = csv_file.data_frame
     tweet_list = csv_data_frame.text.tolist()
     csv_file.pre_process_column(csv_data_frame, tweet_list, column_index)
