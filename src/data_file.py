bcf3d44cd2aa53232ceef123a7682b4f2be8f4bc
Eyüp Pastırmacı <pastirmacieyup7@gmail.com>
2022-03-12 16:50:27 +0300

    Simplify preprocessed data file

--- src/data_file.py
+++ src/data_file.py
@@ -27,7 +27,14 @@ class DataFile:
         for i in range(len(new_tweet_list)):
             new_tweet_list[i] = (self._preprocess.process(new_tweet_list[i]))
         column_number = self._data_frame.columns[replaced_column_index]
-        self._data_frame.drop(column_number, axis=1, inplace=True)
+        self._data_frame.drop("id", axis=1, inplace=True)
+        self._data_frame.drop("user", axis=1, inplace=True)
+        self._data_frame.drop("fullname", axis=1, inplace=True)
+        self._data_frame.drop("url", axis=1, inplace=True)
+        self._data_frame.drop("timestamp", axis=1, inplace=True)
+        self._data_frame.drop("replies", axis=1, inplace=True)
+        self._data_frame.drop("likes", axis=1, inplace=True)
+        self._data_frame.drop("retweets", axis=1, inplace=True)
         self._data_frame[column_number] = new_tweet_list
         self._data_frame.to_csv(self._out_path, encoding=self._encoding)
         print("Ön İşleme Tamamlandı!")
