80e4674ab756c3148f7a56eee029065132e9fc64
Eyüp Pastırmacı <pastirmacieyup7@gmail.com>
2022-03-24 16:51:14 +0300

    Update column method in data file changed to private

--- src/data_file.py
+++ src/data_file.py
@@ -17,6 +17,14 @@ class DataFile:
         self._encoding = encoding
         self._corpus = []
 
+    def __update_column(self, index, new_tweet_list, out_path):
+        """
+        Csv dosyasının belirtilen indeksinde güncelleme yapan metot.
+        """
+        column_number = self._data_frame.columns[index]
+        self._data_frame[column_number] = new_tweet_list
+        self._data_frame.to_csv(out_path, encoding=self._encoding)
+
     def pre_process_column(self, index: int, out_path: str):
         """
         Veri dosyasında ön işleme yapıp yeni dosya olarak kaydeden metot.
@@ -26,25 +34,19 @@ class DataFile:
         for i in range(len(self._data_frame.columns)):
             if i != index:
                 self._data_frame.drop(self._data_frame.columns[i])
-        self.update_column(index, new_tweet_list, out_path)
+        self.__update_column(index, new_tweet_list, out_path)
         print("--- Ön İşleme Tamamlandı ✓ ---")
 
     def extract_roots(self, index: int, out_path: str):
         """
         Kelimelerin Köklerini bularak yeni bir csv dosyası olarak kaydeden metot.
         """
-        self.update_column(index, self._preprocess.get_stem_words(self.tweet_list), out_path)
+        self.__update_column(index, self._preprocess.get_stem_words(self.tweet_list), out_path)
 
     def fix_typos(self, index: int, out_path: str):
         """
         Yazım yanlışlarını düzelterek yeni bir csv dosyası olarak kaydeden metot.
         """
-        self.update_column(index, self._preprocess.fix_typos(self.tweet_list), out_path)
+        self.__update_column(index, self._preprocess.fix_typos(self.tweet_list), out_path)
 
-    def update_column(self, index, new_tweet_list, out_path):
-        """
-        Csv dosyasının belirtilen indeksinde güncelleme yapan metot.
-        """
-        column_number = self._data_frame.columns[index]
-        self._data_frame[column_number] = new_tweet_list
-        self._data_frame.to_csv(out_path, encoding=self._encoding)
+
