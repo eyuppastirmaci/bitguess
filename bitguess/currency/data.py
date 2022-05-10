91c311b9b951977f32427f51beff39ea407d1f7d
Eyüp Pastırmacı <60294196+eyuppastirmaci@users.noreply.github.com>
2022-05-10 05:33:36 +0300

    Currency updates

--- bitguess/currency/data.py
+++ bitguess/currency/data.py
@@ -1,16 +1,19 @@
+import numpy as np
 import pandas as pd
-
 import pandas_datareader as web
 
-import matplotlib.pyplot as plt
-import matplotlib.dates as md
-import datetime as dt
-from collections import namedtuple
+import datetime as dts
 
 
 def fetch_data(path: str, target_currency: str, start: dt.datetime, end: dt.datetime, encoding: str):
     df = web.DataReader(f"BTC-{target_currency}", 'yahoo', start, end)
     df = df.drop(columns=['High', 'Low', 'Open', 'Volume', 'Adj Close'])
+    close_list = df['Close'].tolist()
+    df['change'] = np.array(pd.Series([0.0] + [b - a for a, b in zip(close_list[::1], close_list[1::1])]))
+    df['change_percent'] = np.array(
+        pd.Series([0.0] + [100 * (b - a) / a for a, b in zip(close_list[::1], close_list[1::1])]))
+    df['sentiment'] = np.zeros(len(close_list))
+    df['sentiment_average'] = np.zeros(len(close_list))
     df.to_csv(path, encoding=encoding)
 
 
